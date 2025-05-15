d6f62dac53440422f6ca75094db97046a091d8ec
Aurelien <aurelien78140@outlook.fr>
2025-05-15 20:59:54 +0200

    75

--- porfolio/mon-portfolio/src/App.jsx
+++ porfolio/mon-portfolio/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Hom from './component/hom';
 import Nav from './component/nav';
-import Moi from './component/moi'; // <-- Ajoute cette ligne
+import Moi from './component/moi';
 import ProjectsGrid from './component/ProjectsGrid.jsx';
-import Skill from './component/skill.jsx'
-import Footer from './component/footer.jsx'
+import Skill from './component/skill.jsx';
+import Footer from './component/footer.jsx';
+
 function AppContent() {
   const location = useLocation();
-  const showNavbar = location.pathname === '/'; // Affiche la navbar sur "/"
+  const showNavbar = location.pathname === '/';
 
   return (
     <>
@@ -26,7 +27,6 @@ function AppContent() {
             </>
           }
         />
-        {/* Ajoute ici d'autres routes si besoin */}
       </Routes>
     </>
   );
@@ -34,7 +34,7 @@ function AppContent() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/projet_perso.io"> {/* Ajout du basename */}
       <AppContent />
     </BrowserRouter>
   );
